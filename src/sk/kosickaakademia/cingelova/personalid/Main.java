365ac3b022a873fe12cfa8df4e77c3c1ff928351
JanaCingelova <jana.cingelova@kosickaakademia.sk>
2021-01-12 22:33:15 +0100

    valid numbers

---
+++ src/sk/kosickaakademia/cingelova/personalid/Main.java
@@ -0,0 +1,10 @@
+package sk.kosickaakademia.cingelova.personalid;
+
+import java.lang.ref.PhantomReference;
+
+public class Main {
+    public static void main(String[] args) {
+        PersonalID p =new PersonalID();
+        System.out.println(p.checkId("976231/1240"));
+    }
+}
